046628c1122b1bb3d52c4f254e656ca8287454d2
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-05-18 12:03:48 +0200

    memory_allocation.

--- src/headers/memory_allocation.h
+++ src/headers/memory_allocation.h
@@ -1,13 +1,12 @@
-#include <iostream>
-#include <random>
-#include <string>
-#include <cmath>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <iostream>
+#include <random>
+#include <string>
+#include <cmath>
 
 void memory_allocation_cn(
-    double ***c_n, double ***c, double ***f_n, double ***f,
-    int nx, int ny, int nz
-);
+  double ***c_n, double ***c, double ***f_n, double ***f,
+  int nx, int ny, int nz);
