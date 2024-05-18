03ce265a69755b21f5822a4b1ddc86fbb03e7842
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-05-18 12:14:43 +0200

    initialization_fcts.

--- src/initialization_fcts.cpp
+++ src/initialization_fcts.cpp
@@ -4,25 +4,25 @@ using namespace std;
 
 
 void initialization_fcts(
-    double *advection, double *reaction, double *diffusion, 
-    int nz, 
-    double dz, double AA, double BB, double D_a
-    )
-{
-#pragma omp parallel for simd 
-	for (int k = 0; k < nz; k++)
-	{
-		advection[k] = AA / ((k * dz) * (k * dz) * (k * dz)) - 6.0 * BB / ((k * dz) * (k * dz) * (k * dz) * (k * dz));
-		reaction[k] = 3.0 * AA / ((k * dz) * (k * dz) * (k * dz) * (k * dz)) + 12.0 * BB / ((k * dz) * (k * dz) * (k * dz) * (k * dz) * (k * dz));
-		diffusion[k] = D_a + BB / ((k * dz) * (k * dz) * (k * dz));
-	}
-	// For the first 10 positions (z = [0,10]), assagning the same value
-	double aaa = 10.0 * 10.0 * 10.0;
-	double aaaa = 10.0 * 10.0 * 10.0 * 10.0;
-	for (int k = 0; k * dz < 10; k++)
-	{
-		advection[k] = AA / aaa - 6.0 * BB / aaaa;
-		reaction[k] = 3.0 * AA / aaaa + 12.0 * BB / (aaaa * 20.0);
-		diffusion[k] = D_a + BB / aaa;
-	}
+  double *advection, double *reaction, double *diffusion,
+  int nz,
+  double dz, double AA, double BB, double D_a) {
+#pragma omp parallel for simd
+    for (int k = 0; k < nz; k++) {
+      advection[k] = AA / ((k * dz) * (k * dz) * (k * dz)) \
+        - 6.0 * BB / ((k * dz) * (k * dz) * (k * dz) * (k * dz));
+      reaction[k] = 3.0 * AA / \
+        ((k * dz) * (k * dz) * (k * dz) * (k * dz)) + 12.0 * BB / \
+        ((k * dz) * (k * dz) * (k * dz) * (k * dz) * (k * dz));
+      diffusion[k] = D_a + BB / ((k * dz) * (k * dz) * (k * dz));
+    }
+
+    // For the first 10 positions (z = [0,10]), assagning the same value
+    double aaa = 10.0 * 10.0 * 10.0;
+    double aaaa = 10.0 * 10.0 * 10.0 * 10.0;
+    for (int k = 0; k * dz < 10; k++) {
+      advection[k] = AA / aaa - 6.0 * BB / aaaa;
+      reaction[k] = 3.0 * AA / aaaa + 12.0 * BB / (aaaa * 20.0);
+      diffusion[k] = D_a + BB / aaa;
+    }
 }
