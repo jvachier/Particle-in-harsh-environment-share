82b3d68fd41bd1269440dcd234ab72319c3e7e2f
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-05-18 12:18:03 +0200

    delocate_memory.

--- src/headers/delocate_memory.h
+++ src/headers/delocate_memory.h
@@ -1,13 +1,13 @@
-#include <iostream>
-#include <random>
-#include <string>
-#include <cmath>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
 
+#include <iostream>
+#include <random>
+#include <string>
+#include <cmath>
+
 void delocate_memory(
-    double ***f, double ***f_n, double ***c, double ***c_n,
-    int nx, int ny, int nz
-);
+  double ***f, double ***f_n, double ***c, double ***c_n,
+  int nx, int ny, int nz);
